666107498b4f6a1fdfaa89dc8fe7995b6e8fb378
Pedro Coelho <pedro.coelho@moreharvest.com>
2025-05-07 20:53:47 +0800

    testing passing commit message

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM node:20-alpine AS builder
 
 WORKDIR /app
 
+# add env variable
+ARG VITE_GA_MSG
+ENV VITE_GA_MSG=${VITE_GA_MSG}
+
 COPY package.json .
 
 RUN npm install
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="app-container">
       <h1>AWS deployed from Github Actions</h1>
+      <p>{import.meta.env.VITE_GA_MSG}</p>
     </div>
   );
 }
